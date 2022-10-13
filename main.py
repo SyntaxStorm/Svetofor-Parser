3b6991bac976bb1bf3c9084d3f1fbcc136f04e2a
Haron098 <alkeevvalba@gmail.com>
2022-10-13 21:09:58 +0600

    initional new project

--- main.py
+++ main.py
@@ -16,16 +16,19 @@ def get_data(soup):
     catalog = soup.find('div',class_="grid-list asdads")
     phones = catalog.find_all('div', class_='ty-column4')
     for phone in phones:
-        title = phone.find('a', class_='product-title').text.strip()
-        image = phone.find('img', class_='ty-pict').get('data-ssrc')
-
-        price = phone.find('span', class_="ty-price-update").find('span').text
-        print(price)
-        write_csv({
-            'title': title,
-            'image': image,
-            'price': price
-        })    
+        try:
+            title = phone.find('a', class_='product-title').text.strip()
+            image = phone.find('img', class_='ty-pict').get('data-ssrc')
+            price = phone.find('span', class_="ty-price-update").find('span').text
+            write_csv({
+                'title': title,
+                'image': image,
+                'price': price
+            })
+        except:
+            continue
+      
+        
 
 def write_csv(data):
     with open('phones.csv', 'a') as file:
@@ -36,12 +39,15 @@ def write_csv(data):
 
 
 def main():
-    BASE_URL = 'https://svetofor.info/sotovye-telefony-i-aksessuary/vse-smartfony/smartfony-s-podderzhkoy-4g-ru/'
-    html = get_html(BASE_URL)
-    soup = get_soup(html)
-    get_data(soup)
-
-
-
+    try:
+        for i in range(1, 1000):
+            BASE_URL = f'https://svetofor.info/sotovye-telefony-i-aksessuary/vse-smartfony/smartfony-s-podderzhkoy-4g-ru/page-{i}/'
+            html = get_html(BASE_URL)
+            soup = get_soup(html)
+            get_data(soup)
+            print(f'спарсили {i} страницу ')
+    except AttributeError:
+        print('Конец. Это была последняя страница')
+    
 if __name__ == '__main__':
     main() 
